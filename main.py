d163c619184308cfe853d8136aa6a83b8117fb2c
kayakIYD <ficupaolo@gmail.com>
2018-11-03 18:58:32 +0100

    Fix unsorted plotting to pretty sorted, colorful plotting

--- main.py
+++ main.py
@@ -16,13 +16,18 @@ for c, i in zip(classes, range(len(classes))):
     values = values_processing.append_student_to_value(values_raw)
     values_unsorted = values_processing.append_student_to_value(values_raw_unsorted)
     plt.figure(i)
-    plt.xlabel('Result')
+    plt.xlabel('Student')
+    plt.ylabel('Result')
+    X = np.array(values_raw_unsorted)
+    estimator = KMeans(n_clusters=5).fit(X.reshape(-1, 1))
+    clusters = values_processing.put_values_to_clusters(values_unsorted, estimator.labels_)
+    clusters_sorted = values_processing.sort_clusters(clusters, len(set(estimator.labels_)))
+    clusters_with_tuples_sorted = values_processing.sort_value_in_each_cluster(clusters_sorted)
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+    for item, color in zip(clusters_with_tuples_sorted, colors):
+        for pair in clusters_with_tuples_sorted[item]:
+            plt.scatter(pair[0]+1, pair[1], color=color)
+    plt.xticks(list(plt.xticks()[0]) + [1])
     plt.xlim(left=1)
-    plt.ylabel('Student')
-    X = np.array(values_raw)
-    estimator = KMeans(n_clusters=5, random_state=42).fit(X.reshape(-1, 1))
-    x = [tup[0] for tup in values]
-    y = [tup[1] for tup in values]
-    colors = values_processing.normalize(estimator.labels_)
-    plt.scatter(np.asarray(x), np.asarray(y), c=colors)
     plt.show()
+    kmeans.append(estimator)
