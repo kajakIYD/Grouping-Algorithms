ffbc34d7a22159857fd482f7f1be084af0f8306f
kayakIYD <ficupaolo@gmail.com>
2018-11-03 17:23:08 +0100

    Add values_processing.py

---
+++ values_processing.py
@@ -0,0 +1,35 @@
+# Values processing
+from collections import defaultdict
+
+
+def append_student_to_value(values):
+    values_with_student_number = []
+    for i in range(len(values)):
+        values_with_student_number.append((i, values[i]))
+    return values_with_student_number
+
+
+def put_values_to_clusters(values, labels):
+    clusters_dict = defaultdict(list)
+    for val, l in zip(values, labels):
+        clusters_dict[l].append(val)
+    return clusters_dict
+
+
+def sort_clusters(values_in_clusters, labels_count):
+    temp = []
+    for i in range(labels_count):
+        values_from_cluster = [int(tup[1]) for tup in values_in_clusters[i]]
+        if max(values_from_cluster) > -1:
+            temp.append((i, max(values_in_clusters[i])))
+        else:
+            temp.append((i, None))
+    temp.sort(key=lambda tup: tup[1], reverse=True)
+    sorted_values_in_clusters = {}
+    for i, pos in zip(temp, range(labels_count)):
+        sorted_values_in_clusters[pos] = values_in_clusters[i[0]]
+    return sorted_values_in_clusters
+
+
+def normalize(vector):
+    return [(value - min(vector))/max(vector) for value in vector]
